e93fdb55564001a3b5e8db421df0a35439d66aac
Rhodri Hewitson <rhhewitson@gmail.com>
2021-04-15 20:11:51 +0100

    Return orders connected to the order

--- test/WebAPI.IntegrationTests/CreateDrinkRunTests.cs
+++ test/WebAPI.IntegrationTests/CreateDrinkRunTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using WebAPI.IntegrationTests.ExpectedModels;
 using WebAPI.IntegrationTests.TestUtils;
 using Xunit;
 
@@ -123,25 +124,27 @@ namespace WebAPI.IntegrationTests
 
             var id = new Guid(responseBody.Value<string>("id"));
             var drinkMaker = responseBody.SelectToken("drinkMaker");
+            var orders = responseBody.SelectToken("orders").ToObject<IEnumerable<DrinkOrdersV1>>();
 
             Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
             Assert.NotEqual(Guid.Empty, id);
             Assert.Equal(expectedDrinkMakerId.ToString(), drinkMaker.Value<string>("id"));
             Assert.Equal(expectedDrinkMakerName.firstName, drinkMaker.Value<string>("firstName"));
             Assert.Equal(expectedDrinkMakerName.lastName, drinkMaker.Value<string>("lastName"));
+            
+            Assert.NotNull(orders);
+            Assert.Contains(orders, o => o.UserId == userId);
+            Assert.Contains(orders, o => o.UserId == expectedDrinkMakerId);
+
             Assert.Equal($"http://localhost/v1/DrinkRun/{id}", httpResponse.Headers.Location.ToString());
         }
-
-        
-
+       
         private async Task<Guid> CreateUser(object user, object brew)
         {
             var userId = new Guid((await _client.SendCreateUserRequest(user)).responseBody.Value<string>("id"));
             await _client.SendCreateDrinkOrderRequest(userId, brew);
             return userId;
         }
-
-        
 
     }
 }
